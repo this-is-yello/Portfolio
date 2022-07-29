30d623a1b08c1a291c238873b24aaea037d7b8bc
this-is-yello <yello_3617@naver.com>
2022-07-29 15:05:36 +0900

    220729

--- main.js
+++ main.js
@@ -1,9 +1,45 @@
-const sections = document.getElementsByTagName("section");
+header();
+fadeIn();
+copyToClipboard();
 
-const navBar = document.querySelector(".nav-bar-menu");
+function header() {
+  const sections = document.getElementsByTagName("section");
+  
+  const navBar = document.querySelector(".nav-bar-menu");
+  
+  for (let i = 0; i < navBar.childElementCount; i++) {
+    navBar.children[i].addEventListener("click", () => {
+      window.scrollTo({top: sections[i].offsetTop, behavior: "smooth"});
+      console.log(window.scrollY);
+      console.log(sections[i].offsetTop);
+    });
+  }
+}
 
-for (let i = 0; i < navBar.childElementCount; i++) {
-  navBar.children[i].addEventListener("click", () => {
-    window.scrollTo({top: sections[i].offsetTop, behavior: "smooth"});
+
+function fadeIn() {
+  const sections = document.getElementsByTagName("section");
+  
+  for (let i = 0; i < sections.length; i++) {
+    window.addEventListener("scroll", () => {
+      if (sections[i].getBoundingClientRect().top - (window.innerHeight/2*1) < 0) {
+        sections[i].classList.add("fade-in");
+      }
+    });
+  }
+}
+
+
+function copyToClipboard() {
+  const eMail = document.querySelector(".e-mail");
+  const eMailCopy = document.querySelector(".mail-copy");
+
+  eMail.addEventListener("click", () => {
+    navigator.clipboard.writeText(eMail.innerText);
+    eMailCopy.classList.add("complete-copy");
+
+    setTimeout(() => {
+      eMailCopy.classList.remove("complete-copy");
+    }, 3000)
   });
 }
